4c9e5e44a064d1d5b856e54d347d4d309758ae82
mbarsalou <mbarsalou@lcgalaska.com>
2015-12-08 19:24:49 -0900

    feat: Meet initial tournament requirements
    This commit covers the 8 tests that are paired with the main assignment

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
-
+import re
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -17,22 +17,42 @@ def deleteMatches():
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    myconn = connect()
+    mycursor = myconn.cursor()
+    mycursor.execute('DELETE from players;')
+    rows = mycursor.rowcount
+    myconn.commit()
+    myconn.close()
+    return rows
 
 def countPlayers():
     """Returns the number of players currently registered."""
-
+    myconn = connect()
+    mycursor = myconn.cursor()
+    mycursor.execute('SELECT * from players;')
+    rows = mycursor.rowcount
+    rows = 0 if rows is None else rows
+    myconn.commit()
+    myconn.close()
+    return rows
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
-
+    myconn = connect()
+    mycursor = myconn.cursor()
+    query = mycursor.mogrify(
+            "INSERT INTO players (fullname,wins,matches)" \
+           " VALUES (%s, %s, %s)", (name, 0, 0))
+    mycursor.execute(query)
+    myconn.commit()
+    myconn.close()
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -48,6 +68,13 @@ def playerStandings():
         matches: the number of matches the player has played
     """
 
+    myconn = connect()
+    mycursor = myconn.cursor()
+    mycursor.execute("SELECT * FROM players;")
+    rows = mycursor.fetchall()
+    myconn.commit()
+    myconn.close()
+    return rows
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -56,16 +83,28 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+    myconn = connect()
+    mycursor = myconn.cursor()
+    winnerq = mycursor.mogrify("UPDATE players " \
+            "SET (wins, matches) = (wins+1, matches+1) " \
+            "WHERE playerid = %s", [winner])
+    mycursor.execute(winnerq)
+    loserq = "UPDATE players " \
+            "SET (matches) = (matches+1) " \
+            "WHERE playerid = %s"
+    mycursor.execute(loserq, [loser])
+    myconn.commit()
+    myconn.close()
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +112,18 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+
+    myconn = connect()
+    mycursor = myconn.cursor()
+    mycursor.execute(
+            "SELECT playerid, fullname FROM players ORDER BY wins DESC;")
+    results = mycursor.fetchall()
+    newresults=[]
+    for i in range(0, len(results), 2):
+        newresults.append(list(results[i] + results[i+1]))
+    myconn.commit()
+    myconn.close()
+    return newresults
+
 
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,13 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+Drop Database if exists tournament;
+Create Database tournament;
+\c tournament;
+Create Table players (
+  playerId    serial NOT NULL PRIMARY KEY,
+  fullname    varchar(48) NOT NULL,
+  wins        integer,
+  matches     integer
+);
 
